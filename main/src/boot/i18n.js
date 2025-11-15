375890af7f4bde1e97f72a146119a64dcd5c63cc
Милованова Анастасия Ивановна <aimilovanova@edu.hse.ru>
2025-11-15 21:25:50 +0300

    refactor: Изменение конфигурации

--- main/src/boot/i18n.js
+++ main/src/boot/i18n.js
@@ -2,8 +2,8 @@ import { defineBoot } from '#q-app/wrappers'
 import { createI18n } from 'vue-i18n'
 import messages from 'src/i18n'
 
-export default defineBoot(({ app }) => {
-  const i18n = createI18n({
+export const i18n = createI18n({
+    legacy: false,
     locale: 'en-US',
     fallbackLocale: 'ru-RU',
     messages,
@@ -12,6 +12,7 @@ export default defineBoot(({ app }) => {
       return key
     }
   })
-  
+
+export default defineBoot(({ app }) => {
   app.use(i18n)
 })
